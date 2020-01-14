79b1122c6c5fba0b74d157304a974d91a2ef4af9
DamianLeung <ryo.kihin@gmail.com>
2020-01-14 10:45:07 +0800

    Remove duplicated Dao interfaces 5

--- src/main/java/com/dfbz/controller/BaseServlet.java
+++ src/main/java/com/dfbz/controller/BaseServlet.java
@@ -11,7 +11,12 @@ import java.lang.reflect.Method;
 public class BaseServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
         String uri = req.getRequestURI();
         String[] split = uri.split("/");
         String methodName = split[2];
@@ -26,4 +31,21 @@ public class BaseServlet extends HttpServlet {
             e.printStackTrace();
         }
     }
+
+   /* @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String uri = req.getRequestURI();
+        String[] split = uri.split("/");
+        String methodName = split[2];
+        System.out.println(methodName);
+        String className = this.getClass().getName();
+        System.out.println(className);
+        try {
+            Class<?> aClass = Class.forName(className);
+            Method method = aClass.getMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
+            method.invoke(this, req, resp);
+        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }*/
 }

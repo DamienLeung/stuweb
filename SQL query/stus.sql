c367fd233d3e3937b7dca67421e19e5f94e10390
DamianLeung <ryo.kihin@gmail.com>
2020-01-14 16:13:06 +0800

    Default SQL queries for test update

---
+++ SQL query/stus.sql
@@ -0,0 +1,6 @@
+create table stus(id int primary key, studentN varchar(25), phone varchar(12), addr varchar(30));
+INSERT INTO `stus`(`id`, `studentN`, `phone`, `addr`) VALUES (1001, '張三', '10086', '廣州');
+INSERT INTO `stus`(`id`, `studentN`, `phone`, `addr`) VALUES (1002, 'Damian', '10010', '佛山');
+INSERT INTO `stus`(`id`, `studentN`, `phone`, `addr`) VALUES (1003, '王五', '10086', '深圳');
+INSERT INTO `stus`(`id`, `studentN`, `phone`, `addr`) VALUES (1004, 'damion', '15000', '北京');
+INSERT INTO `stus`(`id`, `studentN`, `phone`, `addr`) VALUES (1005, '李四', '10000', '北京');
---
+++ SQL query/user.sql
@@ -0,0 +1,2 @@
+create table user(username varchar(25) primary key, password varchar(30));
+INSERT INTO `user`(`username`, `password`) VALUES ('admin', '123456');
